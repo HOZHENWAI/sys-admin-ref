96d10d5167a5699fedcb28b849ba29df5acd4198
HOZHENWAI <35206080+HOZHENWAI@users.noreply.github.com>
2021-09-23 01:36:18 +0200

    Create clean-up-tag.sh

---
+++ clean-up-tag.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+"%F"=$1
+
+"%G"=$2
+
+
+if [$2 == 'AUDIOBOOK' ]; then
+
+cp -r "$1" "/book/audio-import"
+
+elif [$2 == 'BOOK']; then
+
+cp -al "$1" "/book/Calibre-import"
+
+elif [$2 == 'LIGHTNOVEL']; then
+
+cp -al "$1" "/book/Library-import"
+
+fi
