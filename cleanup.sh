f2e24294262ddb63a75126fbb3840299a97c4502
HOZHENWAI <35206080+HOZHENWAI@users.noreply.github.com>
2021-09-23 01:16:42 +0200

    added postcleanup ver0

---
+++ cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+"%F"=$1
+
+"%L"=$2
+
+
+if [$2 == 'eBook-tmp' ]; then
+
+cp -al "$1" "/mnt/tank-0/BOOK-COLLECTION/Calibre-import"
+
+elif [$2 == 'lightnovel']; then
+
+cp -al "$1" "/mnt/tank-0/BOOK-COLLECTION/Library-import"
+
+fi
